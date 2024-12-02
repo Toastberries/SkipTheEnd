0c6f99e84c76248418466f576840261903c4e757
Toast <46634118+Toastberries@users.noreply.github.com>
2024-12-02 05:54:57 +0100

    Initial commit

---
+++ src/main/java/xyz/toastberries/skiptheend/SkipTheEnd.java
@@ -0,0 +1,10 @@
+package xyz.toastberries.skiptheend;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkipTheEnd implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // nothing to initialize
+    }
+}
